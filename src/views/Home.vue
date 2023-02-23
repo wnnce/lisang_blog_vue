dd83bcd633f1a271d4003019567630aab464cf39
lisang <ranchangxin@gmail.com>
2023-02-23 09:30:17 +0800

    文章换页时添加加载动画

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,14 @@ import PostItem from "@/components/PostItem.vue";
 import {onMounted, ref} from "vue";
 import {_getArticleListByNormal} from "@/assets/js/api";
 import {useDataStore} from "@/stores/dataStore";
-import {goTop} from "@/assets/js/util";
+import {goTop, isArrayNull, isObjectNull} from "@/assets/js/util";
 
 const articlePageInfo = ref({});
 const pageNum = ref(1);
 const store = useDataStore()
 async function initPageData(){
   goTop();
+  articlePageInfo.value = {}
   articlePageInfo.value = await _getArticleListByNormal(pageNum.value);
   store.articleNum = articlePageInfo.value.total;
 }
@@ -18,9 +19,16 @@ onMounted(() => {
 })
 </script>
 <template>
-  <a-row :gutter="[0, { xs: 8, sm: 8, md: 12, lg: 24 }]" justify="center">
-    <a-col :span="24" v-for="(article, index) in articlePageInfo.list" >
-      <PostItem :key="article.id" :index="index" :article="article" />
+  <a-row :gutter="[0, { xs: 8, sm: 8, md: 12, lg: 24 }]" justify="center" v-if="isObjectNull(articlePageInfo)">
+    <a-col :span="24">
+      <a-card class="card-base" :loading="true">
+        loading...
+      </a-card>
+    </a-col>
+  </a-row>
+  <a-row :gutter="[0, { xs: 8, sm: 8, md: 12, lg: 24 }]" justify="center" v-else>
+    <a-col :span="24" v-for="(article, index) in articlePageInfo.list" :key="article.id" >
+      <PostItem :index="index" :article="article" />
     </a-col>
     <a-pagination :total="articlePageInfo.total" v-model:current="pageNum" size="small"
                   :default-page-size="5" @change="initPageData"></a-pagination>
